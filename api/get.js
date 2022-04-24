a97facdce23f78c28cdfc4f316f7bba049e746d8
shejanshriram007 <46436580+shejanshriram007@users.noreply.github.com>
2022-04-24 20:41:26 +0530

    Create get.js

---
+++ api/get.js
@@ -0,0 +1,8 @@
+ app.get("/users", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
