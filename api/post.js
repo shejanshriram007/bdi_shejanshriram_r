cc814fbc5b42402de4a74e3c372826668b44d09a
shejanshriram007 <46436580+shejanshriram007@users.noreply.github.com>
2022-04-24 20:42:17 +0530

    Create post.js

---
+++ api/post.js
@@ -0,0 +1,8 @@
+app.post("/users", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
