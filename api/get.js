7f118593f9641931d6e7dbda3f18d16e958ceda8
shejanshriram007 <46436580+shejanshriram007@users.noreply.github.com>
2022-04-28 16:19:43 +0530

    Update get.js

--- api/get.js
+++ api/get.js
@@ -1,8 +1,19 @@
- app.get("/users", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if(error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
-});
+const express =require('express')
+const MongoClient=require('mongodb').MongoClient
+const app=express()
+app.use(express.json())
+var database
+
+app.get('/api/users',(req,res)=>{
+  database.collection('users').find({}).toArray((err,result)=>{
+    if (err)throw err
+    res.send(result)
+  })
+})
+app.listen(8080,()=>{
+  MongoClient.connect('mongodb://localhost:27017',{usenewUrlParser:true},(error,result)=>{
+    if(error) throw error
+    database=result.db('contact')
+    console.log('connection sucessful')
+  })
+})
